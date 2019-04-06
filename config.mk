158416c8ef0692a867b9fa82460e7cfc48266f04
Vince Tse <vincetse@users.noreply.github.com>
2019-04-06 02:21:31 +0000

    Update AWS keys

--- config.mk
+++ config.mk
@@ -1,8 +1,8 @@
 stack_name ?= ami-builder
 region ?= us-east-1
 packer_username ?= packer
-subnet_id ?= subnet-078533e137291e536
-vpc_id ?= vpc-0d73667c1af7d7807
+subnet_id ?= subnet-05cff71054d7d97fa
+vpc_id ?= vpc-0e9dcb43eec1aa096
 
 AWS = aws --region $(region)
 EC2 = $(AWS) ec2
